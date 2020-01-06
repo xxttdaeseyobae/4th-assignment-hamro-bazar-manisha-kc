d9e74d8066b10a73257d40ca7f418e2b7826afa6
xxttdaeseyobae <caseymansa@gmail.com>
2020-01-06 13:44:03 +0545

    Terms and condition added

---
+++ app/src/main/java/com/example/hamrobaraz/TermsConditionActivity.java
@@ -0,0 +1,14 @@
+package com.example.hamrobaraz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TermsConditionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_condition);
+    }
+}
