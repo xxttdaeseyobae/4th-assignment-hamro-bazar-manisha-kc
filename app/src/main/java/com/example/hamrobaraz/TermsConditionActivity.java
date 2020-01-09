88f960d8fe78e67b706bcc98729dd38ba1c179e2
xxttdaeseyobae <caseymansa@gmail.com>
2020-01-09 13:14:12 +0545

    terms and condition added

--- app/src/main/java/com/example/hamrobaraz/TermsConditionActivity.java
+++ app/src/main/java/com/example/hamrobaraz/TermsConditionActivity.java
@@ -2,13 +2,61 @@ package com.example.hamrobaraz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.example.hamrobaraz.TermsPage.AdActivity;
+import com.example.hamrobaraz.TermsPage.Safety;
+import com.example.hamrobaraz.TermsPage.Terms;
 
 public class TermsConditionActivity extends AppCompatActivity {
-
+CheckBox chkTerms,chkSafety,chkAd;
+Button btnread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_condition);
+        chkTerms= findViewById(R.id.chkTerms);
+        chkSafety = findViewById(R.id.chkSafety);
+        chkAd= findViewById(R.id.chkAd);
+        btnread= findViewById(R.id.btnread);
+
+        chkTerms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TermsConditionActivity.this, Terms.class));
+            }
+        });
+
+        chkSafety.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TermsConditionActivity.this, Safety.class));
+            }
+        });
+
+        chkAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TermsConditionActivity.this, AdActivity.class));
+            }
+        });
+        btnread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (chkAd.isChecked()&& chkSafety.isChecked() && chkTerms.isChecked()) {
+                    startActivity(new Intent(TermsConditionActivity.this, DashboardActivity.class));
+                }
+                else {
+                    Toast.makeText(TermsConditionActivity.this,"Error",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hamrobaraz/TermsPage/Safety.java
@@ -0,0 +1,16 @@
+package com.example.hamrobaraz.TermsPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hamrobaraz.R;
+
+public class Safety extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safety);
+    }
+}
