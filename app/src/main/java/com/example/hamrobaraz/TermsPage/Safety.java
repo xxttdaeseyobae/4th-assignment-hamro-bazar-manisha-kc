9583eae95d04faf5f2b8188a316dac1763cd51fe
xxttdaeseyobae <caseymansa@gmail.com>
2020-01-09 13:23:45 +0545

    webpages added

--- app/src/main/java/com/example/hamrobaraz/TermsPage/Safety.java
+++ app/src/main/java/com/example/hamrobaraz/TermsPage/Safety.java
@@ -12,5 +12,6 @@ public class Safety extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_safety);
+
     }
 }
