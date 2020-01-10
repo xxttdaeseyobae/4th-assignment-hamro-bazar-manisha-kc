9a62a760ebd376660f30ea9439f5589c8a2cf5ca
xxttdaeseyobae <caseymansa@gmail.com>
2020-01-10 13:56:17 +0545

    Adapters completed

--- app/src/main/java/com/example/hamrobaraz/adapters/ProductAdapter.java
+++ app/src/main/java/com/example/hamrobaraz/adapters/ProductAdapter.java
@@ -1,38 +1,67 @@
 package com.example.hamrobaraz.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.ViewGroup;
-
 import com.example.hamrobaraz.R;
+import com.example.hamrobaraz.module.Products;
+
+import java.util.List;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolders> {
-productsList;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_adapter);
-    }
+List<Products> productsList;
+Context context;
+
+public ProductAdapter(List<Products> productsList,Context context) {
+    this.productsList =productsList;
+    this.context=context;
+}
 
     @NonNull
     @Override
     public ProductAdapter.ProductViewHolders onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.layout_product,parent, false);
+    return new ProductViewHolders(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProductAdapter.ProductViewHolders holder, int position) {
+    final Products products = productsList.get(position);
+
+    holder.tvProductName.setText(products.getProductName());
+    holder.tvPrice.setText(products.getProductPrice());
+        holder.tvUserOrNot.setText(products.getProductUseOrNot());
+        holder.imgProduct.setImageResource(products.getProductImage());
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount() { return productsList.size();
     }
 
-    public class ProductViewHolders {
+
+    public class ProductViewHolders extends RecyclerView.ViewHolder {
+
+    TextView tvProductName, tvPrice, tvUserOrNot;
+    ImageView imgProduct;
+
+
+        public ProductViewHolders(@NonNull View itemView) {
+            super(itemView);
+            tvProductName = itemView.findViewById(R.id.tvProductName);
+            tvPrice = itemView.findViewById(R.id.tvPrice);
+            tvUserOrNot = itemView.findViewById(R.id.tvUserOrNot);
+
+            imgProduct = itemView.findViewById(R.id.imgProduct);
+
+        }
     }
 }
